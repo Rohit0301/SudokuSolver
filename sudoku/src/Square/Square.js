06df3b916de9577d9d4650e28a48e27de85742d6
Rohit0301 <60229265+Rohit0301@users.noreply.github.com>
2021-05-09 01:40:34 +0530

    Update Square.js

--- sudoku/src/Square/Square.js
+++ sudoku/src/Square/Square.js
@@ -1,31 +1,8 @@
-import {React} from 'react'
+import {React,useState} from 'react'
 import './Square.css';
 
 export default function Square({grid,row,col,handleChange}) {
-
-//    let fontcolor="red"
-//    if(row<3 && col<3)
-//     fontcolor="#e1e5ea"
-//    else if(row<3 && col<6)
-//     fontcolor="#e7d4b5"  
-//     else if(row<3 && col<9)
-//     fontcolor="#fff9b0"
-//     else if(row<6 && col<3)
-//     fontcolor="#ff8474"
-//    else if(row<6 && col<6)
-//     fontcolor="bisque"  
-//     else if(row<6 && col<9)
-//     fontcolor="#b0efeb"
-//     else if(row<9 && col<3)
-//     fontcolor="#3edbf0"
-//    else if(row<9 && col<6)
-//     fontcolor="#ddffbc"  
-//     else if(row<9 && col<9)
-//     fontcolor="#f8a1d1"
-    
-   
-  
-  
+ const [color, setcolor] = useState("orange");
     return (
         <>
               <input  
@@ -33,9 +10,9 @@ export default function Square({grid,row,col,handleChange}) {
                   className="cell" 
                   type="text"
                   maxLength="1"
-                  style={{backgroundColor:"white",fontWeight:600,color:"orange"}}
+                  style={{backgroundColor:"white",fontWeight:600,color:color}}
                   value={grid[row][col]===0?0:grid[row][col]}
-                  onChange={(event)=>handleChange(event)}
+                  onChange={(event)=>{handleChange(event); setcolor("green");}}
                   />
         </>
     )
